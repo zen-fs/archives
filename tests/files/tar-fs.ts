dad923c03de5674b847babdab35825f6b7b55342
James Prevett <jp@jamespre.dev>
2026-08-25 12:07:56 -0500

    [WIP] script changes

--- tests/files/tar-fs.ts
+++ tests/files/tar-fs.ts
@@ -1,10 +1,10 @@
-import { configureSingle, fs } from '@zenfs/core';
+import { configureSingle, fs, mounts } from '@zenfs/core';
 import { configure as configureLog, fancy } from 'kerium/log';
 import { readFileSync } from 'node:fs';
-import { resolve } from 'node:path';
+import { join, resolve } from 'node:path';
 import { parseArgs } from 'node:util';
 import { gunzipSync } from 'node:zlib';
-import { Tar } from '../../src/tar/fs.ts';
+import { Tar, type TarFS } from '../../dist/tar/fs.js';
 
 const {
 	values: options,
@@ -14,18 +14,42 @@ const {
 		extra: { short: 'x', type: 'boolean', default: false },
 		verbose: { short: 'v', type: 'boolean', default: false },
 		gzip: { short: 'z', type: 'boolean', default: false },
+		log: { short: 'L', type: 'boolean', default: false },
+		tree: { short: 'T', type: 'boolean', default: false },
+		count: { short: 'C', type: 'boolean', default: false },
 	},
 	strict: true,
 	allowPositionals: true,
 });
 
-configureLog({ level: options.verbose ? 'debug' : 'info', format: fancy({ style: 'ansi', colorize: 'message' }) });
+if (options.log) configureLog({ level: options.verbose ? 'debug' : 'info', format: fancy({ style: 'ansi', colorize: 'message' }) });
 
 const fileData = readFileSync(resolve(import.meta.dirname, name));
 
 const data = options.gzip ? gunzipSync(fileData) : fileData;
 
+const t0 = performance.now();
 await configureSingle({ backend: Tar, data, name });
+console.log('Configured in', Math.round(performance.now() - t0), 'ms');
 
-console.log(fs.readdirSync('/'));
+const tarfs = mounts.get('/') as TarFS;
+const nEntries = tarfs['inodes'].size;
+
+function tree(path: string, depth: number = 0) {
+	const dir = fs.readdirSync(path, { withFileTypes: true });
+	for (const [i, ent] of dir.entries()) {
+		const line = i == dir.length - 1 ? '└' : '├',
+			intent = '│ '.repeat(depth);
+		console.log(intent + line, ent.name);
+		if (ent.isDirectory()) tree(join(path, ent.name), depth + 1);
+	}
+}
+
+if (options.tree) {
+	console.log('/');
+	tree('/');
+}
+
+if (options.count) console.log('tarfs:', nEntries, 'entries');
+
 debugger;
